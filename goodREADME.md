a33b84d656c968517499e39a45bdee7700b39b12
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-02-15 12:12:15 -0600

    added the md file that is going to be ovewritten and the prompt questions

---
+++ goodREADME.md
@@ -0,0 +1,35 @@
+# Your Project Title
+
+
+
+## Description 
+
+
+
+## Table of Contents
+
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+
+## Installation
+
+
+
+## Usage 
+
+
+
+## Credits
+
+
+
+## License
+
+
+
+## Contributing
+
--- index.js
+++ index.js
@@ -1,2 +1,99 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+const questions = [
+    {
+        type: "input",
+        name: "myProject",
+        message: "What is the name of your Project?"
+    }
+    ,
+    {
+        type: "input",
+        name: "myDescription",
+        message: "Give a description of your Project"
+    },
+    // MISSING TABLE OF CONTENTS
+    {
+        type: "input",
+        name: "myInstall",
+        message: "What are the steps required to install your project?"
+    },
+    {   
+        type: "input",
+        name: "myInstructions",
+        message: "Provide instructions and examples for use"
+    },
+    {
+        type: "input",
+        name: "myCollabs",
+        message: "List collaborators and third-party assets"   
+    },
+    {
+        type: "input",
+        name: "myLicense",
+        message: "What liscense did you use?"
+    }];
+
+    //.then(function(answer) {
+    //     console.log(answer)
+
+    // })
+
+inquirer.prompt(questions).then(function (answer) { 
+    console.log(answer); 
+});
+
+function generateREADME(answer) {
+    return `# Your Project Title
+
+    ${answer.myProject}
+
+    ## Description 
+    
+    ${answer.myDescription}
+    
+    ## Table of Contents
+    
+    
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Credits](#credits)
+    * [License](#license)
+    
+    
+    ## Installation
+    
+    ${answer.myInstall}
+    
+    ## Usage 
+    
+    ${answer.myInstructions}
+    
+    ## Credits
+    
+    ${answer.myCollabs}
+    
+    ## License
+    
+    ${answer.Liscence}
+    
+    `
+}
+
+async function init() {
+    console.log("hi")
+    try {
+      const answers = await promptUser();
+  
+      const md = generateREADME(answers);
+  
+      await writeFileAsync("goodREADME.md", md);
+  
+      console.log("Successfully wrote to index.html");
+    } catch(err) {
+      console.log(err);
+    }
+  }
+  
+  init();
